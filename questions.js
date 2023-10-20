6186333b61618f88cc2ac1f87bdb1fdeea126ea5
Khael1 <122359723+Khael1@users.noreply.github.com>
2023-10-20 20:39:19 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What Section Does Ma'am Myrel handle?",
+        answer: "B. TVL-G",
+        options: [
+            "A. TVL-E",
+            "B. TVL-G",
+            "C. TVL-F",
+            "D. HUMSS-A"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does MIL stands for?",
+        answer: "A. Media Information Literacy",
+        options: [
+            "A. Media Information Literacy",
+            "B. Media Information Literature",
+            "C. Media Introduction Literature",
+            "D. Media Introduction Literacy"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does UCSP stands for?",
+        answer: "B. Understanding Culture Society and Politics",
+        options: [
+            "A. Understanding Culture Social and Politics",
+            "B. Understanding Culture Society and Politics",
+            "C. Understanding Cultural Social and Politics",
+            "D. Understanding Cultural Society and Politics"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does EAPP stands for?",
+        answer: "D. English for Academic and Professional Purpose",
+        options: [
+            "A. English for Academic and Philippines Purpose",
+            "B. English for Academic and Philippines Professional",
+            "C. English for Academic and Professional Philippines",
+            "D. English for Academic and Professional Purpose"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does FPL stand for?",
+        answer: "C. Filipino sa Piling Larangan",
+        options: [
+            "A. Filipino sa Pagbasa Larangan",
+            "B. Filipino sa Pananaliksik Larangan",
+            "C. Filipino sa Piling Larangan",
+            "D. Filipino sa Pagsusuri Larangan"
+        ]
+    }
+];
